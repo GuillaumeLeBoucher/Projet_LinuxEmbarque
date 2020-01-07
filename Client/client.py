da99d22516428bc5355974a71c46071fa6de7d97
Guillaume le Boucher <guillaume@Guillaumes-MacBook-Pro.local>
2020-01-07 11:44:35 +0100

    Creation client Test pour connection UDP

---
+++ Client/client.py
@@ -0,0 +1,14 @@
+import socket
+
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+MESSAGE = "Hello, World!"
+
+print("UDP target IP: ", UDP_IP)
+print ("UDP target PORT: ", UDP_PORT)
+print ("Message to send: ", MESSAGE)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+messageByte = MESSAGE.encode()
+sock.sendto(messageByte, (UDP_IP, UDP_PORT))
